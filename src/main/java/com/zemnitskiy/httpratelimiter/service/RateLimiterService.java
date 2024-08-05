37f505d17413f8be6acbb93e3579e927848a8ba3
nzemnytskyi <nikita.zemnytskyi@cengage.com>
2024-08-05 15:31:04 +0300

    Logger added

--- src/main/java/com/zemnitskiy/httpratelimiter/service/RateLimiterService.java
+++ src/main/java/com/zemnitskiy/httpratelimiter/service/RateLimiterService.java
@@ -2,6 +2,8 @@ package com.zemnitskiy.httpratelimiter.service;
 
 import com.zemnitskiy.httpratelimiter.strategy.RateLimiterStrategy;
 import com.zemnitskiy.httpratelimiter.strategy.RateLimiterStrategyFactory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RateLimiterService {
 
   private final RateLimiterStrategyFactory strategyFactory;
+  private final Logger slf4jLogger = Logger.getLogger("RateLimiterService");
   private final ConcurrentHashMap<String, RateLimiterStrategy> clients = new ConcurrentHashMap<>();
 
   public RateLimiterService(RateLimiterStrategyFactory strategyFactory) {
@@ -19,6 +22,8 @@ public class RateLimiterService {
   public boolean isAllowed(String clientKey) {
     RateLimiterStrategy rateLimiter = clients.computeIfAbsent(clientKey,
         _ -> strategyFactory.createStrategy(clientKey));
+    boolean response = rateLimiter.allowRequest();
+    slf4jLogger.log(Level.INFO, clientKey + "Endpoint access " + (response?" Allowed":" Denied"));
     return rateLimiter.allowRequest();
   }
 }
